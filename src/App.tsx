918e0344f40e14f0aeebff323d49208fe328735d
Juanetoh <jureljuan@gmail.com>
2025-02-10 19:13:35 -0800

    vite + react setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className='container'>
+      <h1 className='mainTitle'>JUANETOH</h1>
+    </div>
+  );
+};
+
+export default App
