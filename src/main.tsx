78ca42ce5d55da43d42e70707fe3d4cddc355c56
Juanetoh <jureljuan@gmail.com>
2025-03-04 21:30:24 -0800

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize('G-B7PKGX8CTZ');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
